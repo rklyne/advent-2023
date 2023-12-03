e6ac5e74ae8754ff7ac3fd1fcbeca3e2c2bb5b47
Ronan Klyne <ronan@razorsecure.com>
2023-12-03 08:41:07 +0000

    day 03 part 2

--- day03/problem.py
+++ day03/problem.py
@@ -6,11 +6,11 @@ from data import data, example
 
 
 Input = list[str]
-Span = [int, [int, int]]
+Span = typing.Tuple[int, typing.Tuple[int, int]]
 
 
 def parse(input: str) -> Input:
-    lines = ['.' + l + '.' for l in input.split("\n")]
+    lines = ["." + l + "." for l in input.split("\n")]
     blank = "." * len(lines[0])
     return [blank] + lines + [blank]
 
@@ -18,37 +18,79 @@ def parse(input: str) -> Input:
 digit = re.compile("[0-9]+")
 
 
-def part1(input: Input):
-    numbers: list[Span] = []
+def get_adjacent_ranges(n: Span) -> list[Span]:
+    line = n[0]
+    start = n[1][0]
+    end = n[1][1]
+    return [
+        (line - 1, (start - 1, end + 1)),
+        (line + 1, (start - 1, end + 1)),
+        (line, (start - 1, start)),
+        (line, (end, end + 1)),
+    ]
+
+
+def find_parts(input: Input, searcher: re.Pattern = digit) -> list[Span]:
+    parts: list[Span] = []
     for y, line in enumerate(input):
         upto = 0
         while True:
-            match = digit.search(line, pos=upto)
+            match = searcher.search(line, pos=upto)
             if not match:
                 break
             start = match.span()[0]
             end = match.span()[1]
-            numbers.append((y, (start, end)))
+            parts.append((y, (start, end)))
             upto = end
-    strings: list[str] = []
-    for n in numbers:
-        line = n[0]
-        start = n[1][0]
-        end = n[1][1]
-        string = input[line][start:end]
-        string_adjacent = ''.join([
-            input[line-1][start-1:end+1],
-            input[line+1][start-1:end+1],
-            input[line][start-1:start],
-            input[line][end:end+1],
-        ])
-        if string_adjacent.count(".") != len(string_adjacent):
-            strings.append(string)
-    return sum(int(s) for s in strings)
+    return parts
+
+
+def read_input_span(input: Input, n: Span) -> str:
+    line = n[0]
+    start = n[1][0]
+    end = n[1][1]
+    return input[line][start:end]
+
+
+def filter_parts(input: Input, f: typing.Callable[[str], bool]):
+    parts = find_parts(input)
+    parts_ids: list[str] = []
+    for n in parts:
+        string = read_input_span(input, n)
+        ranges = get_adjacent_ranges(n)
+        string_adjacent = "".join([input[r[0]][r[1][0] : r[1][1]] for r in ranges])
+        if f(string_adjacent):
+            parts_ids.append(string)
+    return parts_ids
+
+
+def span_in_spans(s0: Span, ss: list[Span]) -> bool:
+    for s1 in ss:
+        if s0[0] == s1[0]:
+            r0 = s0[1]
+            r1 = s1[1]
+            if r0[0] >= r1[0] and r0[1] <= r1[1]:
+                return True
+    return False
+
+
+def part1(input: Input):
+    parts = filter_parts(input, lambda adj: adj.count(".") != len(adj))
+    return sum(int(s) for s in parts)
 
 
 def part2(input: Input):
-    return 0
+    parts = find_parts(input)
+    gears = find_parts(input, re.compile('\*'))
+    total_ratio = 0
+    for gear in gears:
+        parts_adjacent = []
+        for part in parts:
+            if span_in_spans(gear, get_adjacent_ranges(part)):
+                parts_adjacent.append(read_input_span(input, part))
+        if len(parts_adjacent) == 2:
+            total_ratio += int(parts_adjacent[0]) * int(parts_adjacent[1])
+    return total_ratio
 
 
 class Tests(unittest.TestCase):
@@ -62,10 +104,10 @@ class Tests(unittest.TestCase):
         self.assertEqual(514969, part1(parse(data)))
 
     def test_part2_example_answer(self):
-        self.assertEqual(-1, part2(parse(example)))
+        self.assertEqual(467835, part2(parse(example)))
 
     def test_part2_answer(self):
-        self.assertEqual(-1, part2(parse(data)))
+        self.assertEqual(78915902, part2(parse(data)))
 
 
 if __name__ == "__main__":
