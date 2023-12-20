99089ceebd11c670bbf01d52ffc09f32d84ea781
Ronan Klyne <ronan@razorsecure.com>
2023-12-20 00:07:38 +0000

    day 19 part 2 with bug

--- day19/problem.py
+++ day19/problem.py
@@ -18,6 +18,8 @@ PuzzleInput = Tuple[list[Flow], list[Input]]
 
 
 class Computer:
+    MAX = 4000 + 1
+    _reversed_flows: Dict[Name, list[Name]]
     letter_idx: Dict[Name, int] = {
         "x": 0,
         "m": 1,
@@ -28,35 +30,21 @@ class Computer:
     def __init__(self, flows: list[Flow]):
         self.flows = flows
         self._flows = {
+            f[0]: f
+            for f in flows
+        }
+        self._flow_functions = {
             f: self._mk_f(cs, d)
             for (f, cs, d) in flows
         }
-        self._reverse = {
-            d: self._mk_reverse(cs, f)
-            for (f, cs, d) in flows
-        }
+        self._reversed_flows = {}
+        for (f, cs, d) in flows:
+            for c in cs:
+                self._reversed_flows.setdefault(c[3], []).append(f)
+            self._reversed_flows.setdefault(d, []).append(f)
 
     @classmethod
     def _mk_f(cls, cs: list[Cond], default: Name) -> Callable[[Input], Name]:
-        matches = []
-        for c in cs:
-            if c[1] == ">":
-                op = lambda n, i=cls.letter_idx[c[0]], x=c[2]: n[i]>x
-            elif c[1] == "<":
-                op = lambda n, i=cls.letter_idx[c[0]], x=c[2]: n[i]<x
-            else:
-                raise RuntimeError(c)
-            matches.append((op, c[3]))
-
-        def f(i: Input) -> Name:
-            for _f, r in matches:
-                if _f(i):
-                    return r
-            return default
-        return f
-
-    @classmethod
-    def _mk_reverse(cls, cs: list[Cond], default: Name) -> Callable[[Input], Name]:
         matches = []
         for c in cs:
             if c[1] == ">":
@@ -81,32 +69,55 @@ class Computer:
                 return sum(instr)
             if pos == "R":
                 return 0
-            pos = self._flows[pos](instr)
+            pos = self._flow_functions[pos](instr)
         raise RuntimeError("bad looping")
 
     def paths_to(self, target, start="in"):
-        MAX = 4000 + 1
         opts = [
-            set(range(1, MAX))
+            set(range(1, self.MAX))
             for c in 'xmas'
         ]
-        todo = [start]
-        while todo:
-            name, flow, default = self._reversed_flows[todo.pop()]
-            for char, op, const, to in flow:
+        return self._paths_to('A', opts, start)
+
+    def _paths_to(self, target: Name, sets_in: list[set[int]], finish: Name):
+        print(f"_paths_to({target}, {[len(s) for s in sets_in]}, {finish})")
+        if target == finish:
+            return reduce(operator.mul, list(map(len, sets_in)), 1)
+        names = self._reversed_flows[target]
+        # TODO:
+        """
+        the plan:
+        - for each flow that ends at X:
+            - (pre inverted from Y: [X] to X: [Y])
+            - for each of the forward conditions from Y:
+                - add recursive call to arrives at Y, with set limited by condition
+                - update my sets to limit by negated condition
+                - add recursion to default by my final sets
+        """
+        total = 0
+        print(f"names[{target}] = {names}")
+        for name in names:
+            if name == "R":
+                continue
+            sets = sets_in
+            _, conds, default = self._flows[name]
+            for char, op, const, to in conds:
                 cidx = self.letter_idx[char]
-                if op == "<":
-                    op_set = set(range(1, const))
+                op_set = set(range(1, const))
+                neg_set = set(range(const+1, self.MAX))
+                if op == ">":
+                    op_set, neg_set = neg_set, op_set
+                if to == target:
+                    new_sets = [(s.intersection(op_set) if i == cidx else s) for i, s in enumerate(sets)]
+                    total += self._paths_to(name, new_sets, finish)
+                    break
+                sets = [(s.intersection(neg_set) if i == cidx else s) for i, s in enumerate(sets)]
+            else:
+                if default == target:
+                    total += self._paths_to(name, sets, finish)
                 else:
-                    op_set = set(range(const+1, MAX))
-                if to == 'R':
-                    opts[cidx].difference_update(op_set)
-                elif to == 'A':
-                    # TODO: Somehtign somethign add to total here if ??? some condition?
-                    opts[cidx].difference_update(op_set)
-                else:
-                    todo.add(to)
-        return reduce(operator.mul, list(map(len, opts)), 1)
+                    raise RuntimeError(target, names, name, conds, default)
+        return total
 
 
 def parse(input: str) -> PuzzleInput:
@@ -143,24 +154,8 @@ def part1(input: PuzzleInput):
 def part2(input: PuzzleInput):
     flows, inputs = input
     computer = Computer(flows)
-    lines_by_char = {
-        'x': [],
-        'm': [],
-        'a': [],
-        's': [],
-    }
-
-    for flow_name, conds, default in flows:
-        for char, op, num, to in conds:
-            if op == ">":
-                num += 1
-            lines_by_char[char].append(num)
-    pairs_by_char = {
-        k: list(zip(v, v[1:]))
-        for k, v in lines_by_char.items()
-    }
-    raise RuntimeError([len(v) for v in lines_by_char.values()])
-
+    result = computer.paths_to('A')
+    return result
 
 
 class Tests(unittest.TestCase):
@@ -180,6 +175,7 @@ class Tests(unittest.TestCase):
     def test_part2_example_answer(self):
         self.assertEqual(167409079868000, part2(parse(example)))
 
+    @unittest.skip
     def test_part2_answer(self):
         self.assertEqual(-1, part2(parse(data)))
 
