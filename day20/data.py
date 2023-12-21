893ba14dd32ae127bb8d03b64413d4fb69e36d18
Ronan Klyne <ronan@razorsecure.com>
2023-12-21 22:22:58 +0000

    day 20 part 2 still janky

--- day20/data.py
+++ day20/data.py
@@ -16,6 +16,68 @@ broadcaster -> a
 
 """.strip()
 
+data2 = """
+%jv -> rn, jn
+%rr -> vm, gp
+%hm -> ql
+%cf -> dx, fb
+%cg -> kl
+%hv -> kg, fb
+%hs -> jv
+%bd -> dt
+%xv -> mv, gp
+%js -> zb, jl
+%rn -> bk, jn
+%lp -> hm
+%dx -> fb, jm
+%ss -> lp
+%bh -> jl, ms
+%km -> jl, lm
+%mv -> gp, qm
+%pt -> jt, jl
+%cb -> bd
+%xt -> jn, jf
+%kg -> fb
+%dg -> jn
+%rt -> fb, hb
+broadcaster -> km, xt, pk, vk
+%lr -> pt
+%vm -> bf
+%hx -> qd, jl
+%hb -> pd
+%vk -> cg, fb
+%kl -> rs
+%pk -> gp, cb
+%jt -> hx, jl
+%bg -> js
+%kz -> ss, jn
+%bf -> fx, gp
+%bk -> dg, jn
+%qm -> rr
+%fx -> gp, dp
+%dp -> gp
+%jf -> jn, kz
+%jm -> hv, fb
+%ql -> hs
+%ms -> bg
+%zb -> lr
+%rs -> fb, rt
+%dt -> xv, gp
+%lm -> bh
+%pd -> cf, fb
+%qd -> jl
+&fb -> hb, vk, fz, kl, cg
+&gp -> vm, cb, bd, qm, xf, pk
+&jl -> km, lm, ms, mp, lr, zb, bg
+&jn -> hs, lp, hm, hn, ql, xt, ss
+&hn -> xn
+&mp -> xn
+&xf -> xn
+&fz -> xn
+&xn -> rx
+
+""".strip()
+
 data = """
 %jv -> rn, jn
 &fb -> hb, vk, fz, kl, cg
--- day20/problem.py
+++ day20/problem.py
@@ -1,4 +1,4 @@
-from typing import Tuple, NewType, Iterable, Dict, Literal
+from typing import Tuple, NewType, Iterable, Dict, Literal, Callable
 import typing
 import unittest
 
@@ -14,6 +14,7 @@ NAND: Op = "&"
 TOGGLE: Op = "%"
 Instr = Tuple[Name, Op, list[Name]]
 Input = list[Instr]
+Pulse = Tuple[Name, Signal, Name]
 
 
 def _parse(input: str) -> Iterable[Instr]:
@@ -43,10 +44,11 @@ class Computer:
     _inputs: Dict[Name, Tuple[Op, list[Name]]]
     _next: Dict[Name, list[Name]]
     _prev: Dict[Name, list[Name]]
-    _pending: list[Tuple[Name, Signal, Name]]
+    _pending: list[Pulse]
     _records: Dict[Name, Tuple[int, int]]
 
-    def __init__(self, inputs: Input):
+
+    def __init__(self, inputs: Input, observer: Callable[[Pulse], None]= None):
         self._inputs = {n: (op, ns) for n, op, ns in inputs}
         self._next = {n: ns for n, op, ns in inputs}
         self._prev = {}
@@ -59,6 +61,7 @@ class Computer:
         self._state = {n: self._initial_state(op, n) for n, op, ns in inputs}
         self._send_high = 0
         self._send_low = 0
+        self.observer = observer
 
     def _initial_state(self, op: Op, name: Name):
         if op == TOGGLE:
@@ -72,10 +75,16 @@ class Computer:
 
     def _send_to(self, target: Name, signal: Signal, src: Name):
         self._pending.append((target, signal, src))
+        if self.observer:
+            self.observer(self._pending[-1])
+        recording = src
+        self._records.setdefault(recording, (0, 0))
         if signal == HIGH:
             self._send_high += 1
+            self._records[recording] = (self._records[recording][0], self._records[recording][1]+1)
         else:
             self._send_low += 1
+            self._records[recording] = (self._records[recording][0]+1, self._records[recording][1])
 
     def process_step(self, trigger: Name = Name("broadcaster"), signal: Signal = LOW):
         self._send_to(trigger, signal, "none")
@@ -106,30 +115,128 @@ class Computer:
             raise RuntimeError("oops", op)
 
     def read(self, n: Name) -> Tuple[int, int]:
-        return self._send_low, self._send_high
         return self._records.get(n, (0, 0))
 
-    def debug(self):
-        return {"loops": self._loops}
+    def reset(self, n: Name) -> None:
+        self._records[n] = (0, 0)
 
+    def read_total(self, n: Name) -> Tuple[int, int]:
+        return self._send_low, self._send_high
+
+    def debug(self, big=False):
+        small = {"loops": self._loops}
+        if not big:
+            return small
+        small.update({
+            "states": self._state
+        })
+        return small
+
+    def dot(self):
+        out: list[str] = []
+        for src, ns in self._next.items():
+            for n in ns:
+                out.append(f"{src} -> {n}")
+        for n in self._inputs:
+            op, _ = self._inputs[n]
+            out.append(f'{n} [label="{op} {n}"]')
+        return "digraph {\n  " + "\n  ".join(out) + "\n}"
 
 def part1(input: Input):
     computer = Computer(input)
-    # computer.process_step()
     for i in range(1000):
         computer.process_step()
-    low, high = computer.read(Name("output"))
-    print(f"loops ({low}, {high}): {computer.debug()}")
+    low, high = computer.read_total(Name("output"))
+    # print(f"loops ({low}, {high}): {computer.debug()}")
     return low * high
 
 
+def steps_to(input: Input, name: str):
+    computer = Computer(input)
+    n = Name(name)
+    c = 0
+    done = False
+    for i in range(1_000_000):
+        computer.process_step()
+        c += 1
+        low, high = computer.read(n)
+        if high:
+            done = True
+            break
+    if not done:
+        raise RuntimeError("overflow")
+    return c
+
+
+def steps_to_each(input: Input, names_in: list[str], seek: Signal) -> Dict[str, list[int]]:
+    names = [Name(n) for n in names_in]
+    c = 0
+
+    tracks: Dict[str,  list[int]] = {n: [] for n in names}
+    loops_to_find = 4
+
+    def observe(signal: Pulse):
+        src, pulse, dest = signal
+        if pulse != seek:
+            return
+        if dest not in tracks:
+            return
+        if len(tracks[dest]) > loops_to_find:
+            return
+        if c not in tracks[dest]:
+            tracks[dest].append(c)
+
+    computer = Computer(input, observer=observe)
+    for i in range(1_000_000):
+        c += 1
+        computer.process_step()
+        if all([len(c) >= loops_to_find for c in tracks.values()]):
+            return tracks
+
+    raise RuntimeError("overflow")
+
+
 def part2(input: Input):
-    return 0
+    c = 0
+    """
+    (4096 - sum(binary_bits)) -> BOOM
+
+    &fb (fz) -> hb, vk, kl, cg
+    &fb (fz) (4057) -> 32,  1,  4,  2
+    &gp (xf) () ->  vm, cb, bd, qm, pk
+    &gp (xf) (3769) -> 256,  2,  4, 64, 1
+    &jl (mp) -> km, lm, ms,  lr, zb, bg
+    &jl (mp) (3877) ->  1,  2,  8, 128, 64, 16
+    &jn (hn) ->  hs, lp, hm, ql, xt, ss
+    &jn (hn) (3847) -> 128, 16, 32, 64,  1,  8
+    """
+
+    # steps = steps_to(input, "fz")
+    steps = steps_to_each(input, [
+        # "fb", "gp", "jl", "jn",
+        "hn", "mp", "xf", "fz",
+    ], HIGH)
+    # steps = steps_to_each(input, [
+    #     "hn", "mp", "xf", "fz",
+    #     "hb", "vk", "kl", "cg",
+    #     "vm", "cb", "bd", "qm", "pk",
+    #     "km", "lm", "ms", "lr", "zb", "bg",
+    #     "hs", "lp", "hm", "ql", "xt", "ss",
+    # ], LOW)
+    print(f"fz: {steps}")
+    # print(f"loops ({low}, {high}): {computer.debug(True)}")
+    return c
 
 
 class Tests(unittest.TestCase):
     def test_parse(self):
         self.assertEqual(("broadcaster", "", ["a", "b", "c"]), parse(example)[0])
+
+    @unittest.skip
+    def test_print_dot(self):
+        print("")
+        print(Computer(parse(data)).dot())
+        print("")
 
     def test_part1_example_answer(self):
         self.assertEqual(32000000, part1(parse(example)))
@@ -140,11 +247,13 @@ class Tests(unittest.TestCase):
     def test_part1_answer(self):
         self.assertEqual(806332748, part1(parse(data)))
 
-    def test_part2_example_answer(self):
-        self.assertEqual(-1, part2(parse(example)))
-
+    # @unittest.skip
     def test_part2_answer(self):
-        self.assertEqual(-1, part2(parse(data)))
+        # too low 695433843
+        # too low 56881971201792
+        # wrong   227583967598592
+        # wrong 29822534917445124096
+        self.assertEqual(228060006554227, part2(parse(data)))
 
 
 if __name__ == "__main__":
