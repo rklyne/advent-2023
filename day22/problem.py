9f3b0389519e04be74e0313004861f2f72a9d233
Ronan Klyne <ronan@razorsecure.com>
2023-12-25 02:56:24 +0000

    day 22 part 2

--- day22/problem.py
+++ day22/problem.py
@@ -2,6 +2,7 @@ from typing import NewType, NamedTuple
 from pprint import pprint
 import typing
 import unittest
+from functools import lru_cache
 
 from data import data, example
 
@@ -44,6 +45,8 @@ def parse(input: str) -> Input:
 class BlockSpace:
     _space: list[list[list[BlockId]]]
     _blocks: dict[BlockId, Block]
+    _supported_by: dict[BlockId, list[BlockId]]
+    _supporting: dict[BlockId, list[BlockId]]
 
     def __init__(self, maxx, maxy, maxz):
         self.maxx = maxx
@@ -51,6 +54,8 @@ class BlockSpace:
         self.maxz = maxz
         self._blank()
         self._blocks = {}
+        self._supported_by = {-1: []}
+        self._supporting = {-1: []}
 
     def _blank(self):
         self._space = [
@@ -61,8 +66,9 @@ class BlockSpace:
     def _set(self, c: Coord, block_id: BlockId):
         self._space[c.x][c.y][c.z] = block_id
 
-    def _cube_fall_dist(self, cube: Coord, ignore: set[BlockId] = set()) -> int:
-        ignores = ignore.union([0])
+    @lru_cache
+    def _cube_fall_dist(self, cube: Coord, ignore: tuple[BlockId] = set()) -> int:
+        ignores = set(ignore).union([0])
         col = self._space[cube.x][cube.y]
         zrange = list(range(cube.z - 1, -1, -1))
         for z in zrange:
@@ -73,26 +79,32 @@ class BlockSpace:
 
     def _block_fall_dist(self, block: Block, ignore: set[BlockId] = set()) -> int:
         dist = min(
-            map(lambda c: self._cube_fall_dist(c, ignore=ignore), self._cubes(block))
+            map(
+                lambda c: self._cube_fall_dist(c, ignore=tuple(ignore)),
+                self._cubes(block),
+            )
         )
-        # print(f"{dist}, {block}")
         return dist
 
-    def could_fall_without(self, block_id: BlockId):
-        ignore = set([block_id])
-        candidates = set()
-        block = self._blocks[block_id]
-        block_top = max(block[0].z, block[1].z)
-        for x in range(0, self.maxx + 1):
-            for y in range(0, self.maxy + 1):
-                candidates.add(self._space[x][y][block_top + 1])
-        candidates.difference_update([0])
-        for other_id in candidates:
-            other_block = self._blocks[other_id]
-            if self._block_fall_dist(other_block, ignore.union(set([other_id]))) != 0:
-                assert self._block_fall_dist(other_block) == 0, f"shouldn't fall without any change? ({other_id})"
-                return True
-        return False
+    def could_fall_without(self, block_ids: list[BlockId], recurse=False):
+        # TODO: filter to blocks this is supporting that now have no support
+        fallers = set([b for b in self._supporting[block_ids[-1]] if not set(self._supported_by[b]).difference(block_ids)])
+        if recurse:
+            for f in list(fallers):
+                fallers.update(self.could_fall_without(block_ids + list(fallers) + [f], True))
+        return fallers
+
+    def _blocks_in_space(self, block: Block, at_z: int) -> set[BlockId]:
+        blocks = set()
+
+        def _mk_range(a, b):
+            i, j = sorted([a, b])
+            return i, j+1
+
+        for x in range(*_mk_range(block[0].x, block[1].x)):
+            for y in range(*_mk_range(block[0].y, block[1].y)):
+                blocks.add(self._space[x][y][at_z])
+        return blocks.difference([0])
 
     @staticmethod
     def _cubes(block: Block) -> list[Coord]:
@@ -129,6 +141,12 @@ class BlockSpace:
             self._set(cube, block_id)
         # print(f">>> added {block_id} @{block}")
         self._blocks[block_id] = block
+        self._supporting[block_id] = []
+        self._supported_by[block_id] = list(self._blocks_in_space(
+            block, min(block[0].z, block[1].z) - 1
+        ))
+        for id in self._supported_by[block_id]:
+            self._supporting[id].append(block_id)
 
     def _zrange(self):
         return range(0, self.maxz)
@@ -137,7 +155,7 @@ class BlockSpace:
         for layer in zip(*map(lambda ls: zip(*ls), self._space)):
             if not any(map(any, layer)):
                 continue
-            print("\n".join([' '.join([str(i).zfill(3) for i in l]) for l in layer]))
+            print("\n".join([" ".join([str(i).zfill(3) for i in l]) for l in layer]))
             print("---")
 
 
@@ -155,21 +173,30 @@ def part1(input: Input):
     for block_id, block in _blocks_with_ids(blocks):
         space.add_block(block, block_id)
 
-    space.pprint()
-
+    # print(f"{space._supported_by}")
+    # print(f"{space._supporting}")
     count = 0
-    removed = []
     for remove_block, block in _blocks_with_ids(blocks):
-        if not space.could_fall_without(remove_block):
+        if not space.could_fall_without([remove_block]):
             count += 1
-            removed.append(remove_block)
-    print(f"blast: {removed}")
 
     return count
 
 
 def part2(input: Input):
-    return 0
+    blocks = list(sorted(input, key=lambda b: min(b[0].z, b[1].z)))
+    xs, ys, zs = zip(*([b[0] for b in blocks] + [b[1] for b in blocks]))
+
+    space = BlockSpace(max(xs), max(ys), max(zs))
+    for block_id, block in _blocks_with_ids(blocks):
+        space.add_block(block, block_id)
+
+    count = 0
+    for remove_block, block in _blocks_with_ids(blocks):
+        fallers = len(space.could_fall_without([remove_block], recurse=True))
+        count += fallers
+
+    return count
 
 
 class Tests(unittest.TestCase):
@@ -185,13 +212,13 @@ class Tests(unittest.TestCase):
         # 544 too high
         self.assertEqual(451, part1(parse(data)))
 
-    @unittest.skip
     def test_part2_example_answer(self):
-        self.assertEqual(-1, part2(parse(example)))
+        self.assertEqual(7, part2(parse(example)))
 
-    @unittest.skip
+    # @unittest.skip
     def test_part2_answer(self):
-        self.assertEqual(-1, part2(parse(data)))
+        # takes a minute but it's right
+        self.assertEqual(66530, part2(parse(data)))
 
 
 if __name__ == "__main__":
